d66d85c4f2566918ecae24823835dd53d575eeba
Miguel A. Zapata <miguel.zapata@adevinta.com>
2020-09-30 11:37:51 -0300

    Apply platform version

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "OHHTTPStubs",
+    platforms: [
+      .iOS(.v10)
+    ],
     products: [
         .library(
             name: "OHHTTPStubs",
